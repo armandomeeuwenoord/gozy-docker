282d6bf2b923390a45f49666c0d9ab5dbb7b0342
Armando Meeuwenoord <armando.meeuwenoord@gmail.com>
2017-11-26 00:04:39 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,15 @@
 echo "⋅ Starting CouchDB…"
 sudo -b -i -u couchdb sh -c '/home/couchdb/bin/couchdb >> /var/log/couchdb/couch.log 2>> /var/log/couchdb/couch-err.log'
 sleep 10
-read -sp "Server Passphrase: " pass
-export COZY_ADMIN_PASSWORD=$pass
+
+
+export public_port=1443
+export server_port=8080
+export instance_domain=codingexperts.nl
+export COZY_ADMIN_PASSWORD=asd3eeiuhasiuhd83e
+export server_pass=asd3eeiuhasiuhd83e
+
+
 echo -e "$COZY_ADMIN_PASSWORD\n$COZY_ADMIN_PASSWORD" | /usr/local/bin/cozy-stack config passwd /etc/cozy/ > /dev/null
 #for pid in $(pgrep cozy-stack); do kill -15 $pid;done
 echo "Starting Cozy stack…"
@@ -11,10 +18,6 @@ sudo -b -u cozy sh -c '/usr/local/bin/cozy-stack serve --log-level debug --host 
 #cozy-stack instances destroy "cozy.tools:8080" 2>/dev/null
 sleep 10
 echo "⋅ Creating instance…"
-read -p "Public port : " -i 1443 -e public_port
-read -p "Instance domain : " -i "cozy.tools" -e instance_domain
-read -p "Server port : " -i 8080 -e server_port
-read -sp "Instance passphrase: " server_pass
 echo "Creating instance"
 cozy-stack instances add --host 0.0.0.0 --apps drive,photos,collect,settings,onboarding --passphrase "$server_pass" "${instance_domain}:${public_port}"
 echo "⋅ Creating certificate…"
